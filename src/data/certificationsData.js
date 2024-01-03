cc40490a32196eba326b25f24a7c842e5ec8c7ea
Jehangir Khan <ijehangirkhan@gmail.com>
2024-01-03 23:37:07 +0500

    GCP Certification Added.

--- src/data/certificationsData.js
+++ src/data/certificationsData.js
@@ -5,19 +5,26 @@ export const certificationsData = {
             id : 1,
             title : 'AWS Certified Solutions Architect – Associate',
             details : 'This certification validate your expertise and showcase your ability to build secure and robust solutions using architectural design principles based on customer requirement.',
-            date : 'Jan, 2022',
+            date : 'Jan, 2022 - Jan, 2025',
             field : 'Cloud',
             image : '/AWS.png'
         },
         {
             id : 2,
-            title : 'HashiCorp Certified: Terraform Associate',
+            title : 'HashiCorp Certified Terraform Associate',
             details : 'Earners of this certification know the basic concepts, skills, and use cases associated with open source HashiCorp Terraform',
-            date : 'Jan, 2023',
+            date : 'Jan, 2023 - Jan 2025',
             field : 'IaC',
             image : '/Terraform.png'
         },
-
+        {
+            id : 3,
+            title : 'Google Cloud Certified Associate Cloud Engineer',
+            details : 'An Associate Cloud Engineer deploys applications, monitors operations, and manages enterprise solutions.',
+            date : 'Dec, 2023 - Dec, 2026',
+            field : 'Cloud',
+            image : '/GCP.png'
+        },
     ]
 }
 
