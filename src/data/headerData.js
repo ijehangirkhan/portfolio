09489b3ee5c5e78b85175e6aa4797bd10e4a34ca
Jehangir Khan <ijehangirkhan@gmail.com>
2024-01-18 17:51:57 +0500

    Picture Changed.

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Jehangir Khan',
     title: "DevOps Engineer",
     desciption:"There wasn't a bird in the sky, but that was not what caught his attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. He knew what was coming and he hoped he was prepared.",
-    image: '/Jehangir.jpg',
+    image: '/Jehangir.png',
     resumePdf: resume
 }
