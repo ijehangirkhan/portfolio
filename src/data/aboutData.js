fa91fcd7f89e8c79f3f86ab8752222e0651255c9
Jehangir Khan <ijehangirkhan@gmail.com>
2024-10-15 12:11:49 +0100

    Updates location

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "My name is Jehangir Khan. I'm a DevOps Engineer based in Islamabad, Pakistan.",
+    description1: "My name is Jehangir Khan. I'm a DevOps Engineer based in Berlin, Germany.",
     description2: "I have 3+ years of hands-on experience supporting, automating, and optimizing mission critical deployments in AWS and GCP, leveraging configuration management, CI/CD, and DevOps processes.",
     image: 1
 }
--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,7 +1,7 @@
 export const contactsData = {
     email: 'hi@jehangir.me',
-    phone: '+1 (669) 2281363',
-    address: 'Islamabad, Pakistan',
+    // phone: '+1 (669) 2281363',
+    address: 'Berlin, Germany',
 
     sheetAPI: 'https://sheetdb.io/api/v1/nagcmvdkygser'
 }
